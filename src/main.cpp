b6bd0d2376b0dbb2fc82158ef90387262de1d969
Fabian Bläse <fabian@blaese.de>
2024-12-30 12:52:09 +0100

    init

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+U8G2_SSD1322_NHD_256X64_1_3W_SW_SPI u8g2(U8G2_R0, 14, 13, 2, 4);
+
+void setup() {
+  Serial.begin(115200);
+
+  u8g2.begin();
+}
+
+void loop() {
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_inb63_mn);
+    u8g2.drawStr(0,64,"0123456");
+  } while ( u8g2.nextPage() );
+
+  delay(1000);
+}
+
+//SSD1322
